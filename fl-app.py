1f5a3a586dd3b92984ec3a12edd93d745d9ce50a
AceTwin <cara.cornel@yahoo.com>
2018-09-18 23:05:51 +0000

    WORKING!

--- fl-app.py
+++ fl-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 from flask_api import FlaskAPI
-from flask import request
+from flask import Flask, request, render_template
 import sys, time
 import RPi.GPIO as GPIO
 
@@ -12,31 +12,40 @@ GPIO.setup(18, GPIO.OUT) #GPIO pin # is 18 -- Red
 GPIO.setup(8, GPIO.OUT) #GPIO pin # is 8 -- Yellow
 GPIO.setup(21, GPIO.OUT) #GPIO pin # is 21 -- Green
 
-app = FlaskAPI(__name__)
+#app = FlaskAPI(__name__)
+app = Flask(__name__)
 
 #Initial Stage -- everything off
 GPIO.output(18, False) #Red is off
 GPIO.output(8, False) #Yellow is off
 GPIO.output(21, False) #Green is off
 
+@app.route('/')
+def root():
+    return render_template('/index.html')
+
 #Red
 @app.route('/redled') #web route to red led
 def redled():
     GPIO.output(18, True) #Red is on
+    return '<meta http-equiv="Refresh" content="0; url=/">Command Sent'
 
 @app.route('/yellowled') #web route to yellow led
 def yellowled():
     GPIO.output(8, True) #Yellow is on
+    return '<meta http-equiv="Refresh" content="0; url=/">Command Sent'
 
 @app.route('/greenled') #web route to green led
 def greenled():
     GPIO.output(21, True) #Green is on
+    return '<meta http-equiv="Refresh" content="0; url=/">Command Sent'
 
 @app.route('/off') #web route to turning light(s) off
 def off():
     GPIO.output(18, False) #Red is off
     GPIO.output(8, False) #Yellow is off
     GPIO.output(21, False) #Green is off
+    return '<meta http-equiv="Refresh" content="0; url=/">Command Sent'
 
 @app.route('/cycle') #web route to cycle through all the lights
 def cycle():
@@ -54,6 +63,7 @@ def cycle():
         GPIO.output(8, True) #Yellow is on
         time.sleep(4) #On for 4 sec
         GPIO.output(8, False) #Yellow is off
+        return '<meta http-equiv="Refresh" content="0; url=/">Command Sent'
 
 if __name__ == "__main__":
     app.run()
