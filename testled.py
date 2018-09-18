aead56ff549ccf4939032e8f40706de172f50f7c
AceTwin <cara.cornel@yahoo.com>
2018-09-18 21:10:39 +0000

    testing 1 led - if it turns on or not

--- testled.py
+++ testled.py
@@ -1,6 +1,16 @@
 import sys, time
 import RPi.GPIO as GPIO
 
-redpin = 11
-greenpin = 13
-bluePin = 15
+#Testing for 1 LED
+
+GPIO.setwarnings(False) #Initial State
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT) #GPIO pin # is 18.
+
+#Test for blinking LED
+
+while True:
+    GPIO.output(18, True)
+    time.sleep(1) #On for 1 sec
+    GPIO.output(18, False)
+    time.sleep(1) #Off for 1 sec
